e0982f628a41114e0ddf2c9e2323f9b9ce43c999
dyuproject@gmail.com <dyuproject@gmail.com@a19f40fd-8e45-0410-b92a-0965b367a759>
2008-12-08 14:44:44 +0000

    ugh, update package name from com.dyuproject.web.mvc to com.dyuproject.web.rest

--- trunk/modules/demos/deprecated/helloworld/src/main/java/com/dyuproject/demos/deprecated/helloworld/MyDefaultController.java
+++ trunk/modules/demos/deprecated/helloworld/src/main/java/com/dyuproject/demos/deprecated/helloworld/MyDefaultController.java
@@ -20,7 +20,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.dyuproject.web.mvc.dispatcher.VelocityDispatcher;
+import com.dyuproject.web.rest.dispatcher.VelocityDispatcher;
 import com.dyuproject.web.rest.mvc.AbstractController;
 
 /**
