1271fe39cf7f5c31fc9e2c1a8265baa4b6863115
dyuproject <dyuproject@a19f40fd-8e45-0410-b92a-0965b367a759>
2009-02-23 14:49:14 +0000

    convertor tests

---
+++ trunk/modules/ioc/src/test/java/com/dyuproject/ioc/OverloadPojoConvertorTest.java
@@ -0,0 +1,50 @@
+//========================================================================
+//Copyright 2007-2009 David Yu dyuproject@gmail.com
+//------------------------------------------------------------------------
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at 
+//http://www.apache.org/licenses/LICENSE-2.0
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+//========================================================================
+
+package com.dyuproject.ioc;
+
+import junit.framework.TestCase;
+
+import com.dyuproject.ioc.test.OverloadTask;
+import com.dyuproject.ioc.test.Task;
+
+/**
+ * @author David Yu
+ * @created Feb 23, 2009
+ */
+
+public class OverloadPojoConvertorTest extends TestCase
+{
+    
+    public void testOverloadTask() throws Exception
+    {
+        String resource = "src/test/resources/com/dyuproject/ioc/test/overload_task.json";
+        ApplicationContext ac = ApplicationContext.load(resource);
+        Task task = (Task)ac.findPojo("task");
+        assertTrue(task!=null);
+        assertTrue("10".equals(task.getStatus()));
+        
+        OverloadTask otask = (OverloadTask)ac.findPojo("overload_task");
+        assertTrue(otask!=null);
+        assertTrue(otask.getStatus()==null);
+        assertTrue(otask.getStatusInt()==5);
+        
+        OverloadTask otask1 = (OverloadTask)ac.findPojo("overload_task1");
+        assertTrue(otask1!=null);
+        assertTrue(otask1.getStatus()==null);
+        assertTrue(otask1.getStatusPojo()!=null);
+        assertTrue("7.5".equals(otask1.getStatusPojo().getId()));
+    }
+
+}
