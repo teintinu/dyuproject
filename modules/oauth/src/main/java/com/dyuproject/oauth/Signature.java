a25eb9624bd0a330ba60ff40b691cea7d74a9b35
dyuproject <dyuproject@a19f40fd-8e45-0410-b92a-0965b367a759>
2009-06-06 16:54:22 +0000

    oauth impl for latest spec (http://oauth.googlecode.com/svn/spec/core/1.0a/drafts/3/oauth-core-1_0a.html)

---
+++ modules/oauth/src/main/java/com/dyuproject/oauth/Signature.java
@@ -0,0 +1,408 @@
+//========================================================================
+//Copyright 2007-2009 David Yu dyuproject@gmail.com
+//------------------------------------------------------------------------
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at 
+//http://www.apache.org/licenses/LICENSE-2.0
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+//========================================================================
+
+package com.dyuproject.oauth;
+
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+
+import com.dyuproject.util.B64Code;
+import com.dyuproject.util.ClassLoaderUtil;
+import com.dyuproject.util.Delim;
+import com.dyuproject.util.http.UrlEncodedParameterMap;
+
+/**
+ * Signature - for signing and verification of oauth requests
+ * 
+ * @author David Yu
+ * @created May 30, 2009
+ */
+
+public abstract class Signature
+{
+    
+    static final String ENCODED_EQ = "%3D";
+    static final String ENCODED_AMP = "%26";
+    
+    static final String[] DEFAULT_OAUTH_TO_SIGN = new String[]{
+        Constants.OAUTH_VERSION,
+        Constants.OAUTH_CONSUMER_KEY,
+        Constants.OAUTH_NONCE,
+        Constants.OAUTH_TIMESTAMP,
+        Constants.OAUTH_SIGNATURE_METHOD
+    };
+    
+    private static final Map<String,Signature> __defaults = new HashMap<String,Signature>(5);
+    
+    public static void register(Signature sig)
+    {
+        __defaults.put(sig.getMethod(), sig);
+    }
+    
+    public static Signature get(String method)
+    {
+        return __defaults.get(method);
+    }
+    
+    /* From UrlEncoded snippet customized to skip {'-', '.', '_', '~'} */
+    /** Perform URL encoding.
+     * @param string 
+     * @return encoded string.
+     */
+    public static String encodeString(String string)
+    {
+        byte[] bytes=null;
+        try
+        {
+            bytes=string.getBytes(Constants.ENCODING);
+        }
+        catch(UnsupportedEncodingException e)
+        {
+            // Log.warn(LogSupport.EXCEPTION,e);
+            bytes=string.getBytes();
+        }
+        
+        int len=bytes.length;
+        byte[] encoded= new byte[bytes.length*3];
+        int n=0;
+        boolean noEncode=true;
+        
+        for (int i=0;i<len;i++)
+        {
+            byte b = bytes[i];
+            
+            if (b==' ')
+            {
+                noEncode=false;
+                //encoded[n++]=(byte)'+';
+                encoded[n++]=(byte)'%';
+                encoded[n++]=(byte)'2';
+                encoded[n++]=(byte)'0';
+            }
+            else if (b>='a' && b<='z' ||
+                     b>='A' && b<='Z' ||
+                     b>='0' && b<='9')
+            {
+                encoded[n++]=b;
+            }
+            else
+            {
+                switch(b)
+                {
+                    case '-':
+                    case '.':
+                    case '_':
+                    case '~':
+                        encoded[n++] = b;
+                        continue;                        
+                }
+                noEncode=false;
+                encoded[n++]=(byte)'%';
+                byte nibble= (byte) ((b&0xf0)>>4);
+                if (nibble>=10)
+                    encoded[n++]=(byte)('A'+nibble-10);
+                else
+                    encoded[n++]=(byte)('0'+nibble);
+                nibble= (byte) (b&0xf);
+                if (nibble>=10)
+                    encoded[n++]=(byte)('A'+nibble-10);
+                else
+                    encoded[n++]=(byte)('0'+nibble);
+            }
+        }
+
+        if (noEncode)
+            return string;
+        
+        try
+        {    
+            return new String(encoded,0,n,Constants.ENCODING);
+        }
+        catch(UnsupportedEncodingException e)
+        {
+            // Log.warn(LogSupport.EXCEPTION,e);
+            return new String(encoded,0,n);
+        }
+    }
+    
+    public static String getKey(String consumerSecret, String secret)
+    {   
+        StringBuilder buffer = new StringBuilder()
+            .append(encodeString(consumerSecret))
+            .append('&');
+        
+        return secret==null ? buffer.toString() : buffer.append(encodeString(secret)).toString();
+    }
+    
+    public static String getBase(UrlEncodedParameterMap params, String method)
+    {
+        List<String> base = new ArrayList<String>();
+        
+        // oauth_token parameter
+        String tokenValue = params.remove(Constants.OAUTH_TOKEN);
+        if(tokenValue!=null)
+        {
+            tokenValue = encodeString(tokenValue);
+            base.add(new StringBuilder()
+                .append(Constants.OAUTH_TOKEN)
+                .append(ENCODED_EQ)
+                .append(tokenValue)
+                .toString());
+        }
+        
+        // default oauth parameters
+        for(String key : DEFAULT_OAUTH_TO_SIGN)
+        {
+            String value = encodeString(params.remove(key));
+            base.add(new StringBuilder()
+                .append(key)
+                .append(ENCODED_EQ)
+                .append(value)
+                .toString());
+        }
+        
+        // request parameters
+        for(Map.Entry<String, String> entry : params.entrySet())
+        {
+            String key = entry.getKey();
+            String value = encodeString(entry.getValue());
+            base.add(new StringBuilder()
+                .append(key)
+                .append(ENCODED_EQ)
+                .append(encodeString(value))
+                .toString());
+        }
+        
+        Collections.sort(base);
+        StringBuilder buffer = new StringBuilder()
+            .append(method)
+            .append('&')
+            .append(encodeString(params.getUrl()))
+            .append('&');
+        
+        for(String b : base)
+            buffer.append(b).append(ENCODED_AMP);
+        
+        return buffer.substring(0, buffer.length()-ENCODED_AMP.length());
+    }
+    
+    public static String getMacSignature(String secretKey, String base, String algorithm)
+    {
+        try
+        {
+            SecretKeySpec sks = new SecretKeySpec(secretKey.getBytes(Constants.ENCODING), algorithm);
+            Mac m = Mac.getInstance(sks.getAlgorithm());
+            m.init(sks);
+            return new String(B64Code.encode(m.doFinal(base.getBytes(Constants.ENCODING))));
+        }
+        catch(Exception e)
+        {
+            throw new RuntimeException(e);
+        }            
+    }
+    
+    public abstract String getMethod();
+    
+    public int hashCode()
+    {
+        return getMethod().hashCode();
+    }
+    
+    protected String getBaseAndPutDefaults(UrlEncodedParameterMap params, Transport transport, 
+            StringBuilder oathBuffer, StringBuilder requestBuffer)
+    {        
+        String url = params.getUrl();
+        int idx = url.indexOf('?');
+        if(idx!=-1)
+        {
+            String[] pairs = Delim.AMPER.split(url.substring(idx+1));
+            for(String pair : pairs)
+            {
+                int eq = pair.indexOf('=');
+                if(eq==-1)
+                    params.put(pair, "");
+                else
+                {
+                    params.put(pair.substring(0, eq), UrlEncodedParameterMap.decode(
+                            pair.substring(eq+1), Constants.ENCODING));
+                }
+            }
+            params.setUrl(url.substring(0, idx));
+        }
+        
+        params.put(Constants.OAUTH_SIGNATURE_METHOD, getMethod());
+        
+        List<String> base = new ArrayList<String>();
+        
+        // oauth_token parameter
+        String tokenValue = params.remove(Constants.OAUTH_TOKEN);
+        if(tokenValue!=null)
+        {
+            tokenValue = encodeString(tokenValue);
+            transport.handleOAuthParameter(Constants.OAUTH_TOKEN, tokenValue, oathBuffer);
+            base.add(new StringBuilder()
+                .append(Constants.OAUTH_TOKEN)
+                .append(ENCODED_EQ)
+                .append(tokenValue)
+                .toString());
+        }
+        
+        // default oauth parameters
+        for(String key : DEFAULT_OAUTH_TO_SIGN)
+        {
+            String value = encodeString(params.remove(key));
+            transport.handleOAuthParameter(key, value, oathBuffer);
+            base.add(new StringBuilder()
+                .append(key)
+                .append(ENCODED_EQ)
+                .append(value)
+                .toString());
+        }
+        
+        // request parameters
+        for(Map.Entry<String, String> entry : params.entrySet())
+        {
+            String key = entry.getKey();
+            String value = encodeString(entry.getValue());
+            if(requestBuffer!=null)
+                transport.handleRequestParameter(key, value, requestBuffer);
+            base.add(new StringBuilder()
+                .append(key)
+                .append(ENCODED_EQ)
+                .append(encodeString(value))
+                .toString());
+        }
+        
+        Collections.sort(base);
+        StringBuilder buffer = new StringBuilder()
+            .append(transport.getMethod())
+            .append('&')
+            .append(encodeString(params.getUrl()))
+            .append('&');
+        
+        for(String b : base)
+            buffer.append(b).append(ENCODED_AMP);
+        
+        return buffer.substring(0, buffer.length()-ENCODED_AMP.length());
+    }
+    
+    public abstract String sign(String consumerSecret, String tokenSecret, String base);
+    
+    public abstract boolean verify(String consumerSecret, String tokenSecret, String method, 
+            UrlEncodedParameterMap params);
+    
+    public abstract void generate(UrlEncodedParameterMap params, String consumerSecret, Token token,
+            Transport transport, StringBuilder oauthBuffer, StringBuilder requestBuffer);
+    
+    
+    public static Signature PLAINTEXT = new Signature()
+    {
+        
+        public String getMethod()
+        {
+            return "PLAINTEXT";
+        }
+        
+        public String sign(String consumerSecret, String tokenSecret, String base)
+        {
+            return getKey(consumerSecret, tokenSecret);
+        }
+
+        public boolean verify(String consumerSecret, String tokenSecret, String method, 
+                UrlEncodedParameterMap params)
+        {
+            String sig = params.get(Constants.OAUTH_SIGNATURE);
+            if(sig==null)
+                return false;
+            
+            int idx = sig.indexOf('&');
+            if(idx==-1)
+                return false;
+            
+            return consumerSecret.equals(sig.substring(0, idx)) && (tokenSecret==null ||
+                    tokenSecret.equals(sig.substring(idx+1)));     
+        }
+        
+        public void generate(UrlEncodedParameterMap params, String consumerSecret, Token token,
+                Transport transport, StringBuilder oauthBuffer, StringBuilder requestBuffer)
+        {
+            getBaseAndPutDefaults(params, transport, oauthBuffer, requestBuffer);
+            String sig = sign(consumerSecret, token.getSecret(), null);
+            transport.handleOAuthParameter(Constants.OAUTH_SIGNATURE, sig, oauthBuffer);
+        }
+        
+    };
+    
+    public static Signature HMACSHA1 = new Signature()
+    {
+
+        public String getMethod()
+        {
+            return "HMAC-SHA1";
+        }
+        
+        public String sign(String consumerSecret, String tokenSecret, String base)
+        {
+            return getMacSignature(getKey(consumerSecret, tokenSecret), base, "HMACSHA1");
+        }
+
+        public boolean verify(String consumerSecret, String tokenSecret, String method, 
+                UrlEncodedParameterMap params)
+        {            
+            String sig = params.remove(Constants.OAUTH_SIGNATURE);        
+            return sig!=null && sig.equals(sign(consumerSecret, tokenSecret, getBase(params, method)));
+        }
+        
+        public void generate(UrlEncodedParameterMap params, String consumerSecret, Token token,
+                Transport transport, StringBuilder oauthBuffer, StringBuilder requestBuffer)
+        {
+            String base = getBaseAndPutDefaults(params, transport, oauthBuffer, requestBuffer);
+            String sig = sign(consumerSecret, token.getSecret(), base);
+            transport.handleOAuthParameter(Constants.OAUTH_SIGNATURE, encodeString(sig), oauthBuffer);
+        }
+        
+    };
+    
+    static
+    {
+        register(PLAINTEXT);
+        register(HMACSHA1);
+        String ext = System.getProperty("oauth.signatures.ext");
+        if(ext!=null)
+        {
+            try
+            {
+                StringTokenizer tokenizer = new StringTokenizer(ext, ",;");
+                while(tokenizer.hasMoreTokens())
+                {
+                    register((Signature)ClassLoaderUtil.newInstance(tokenizer.nextToken().trim(), 
+                            Signature.class));
+                }
+            }
+            catch(Exception e)
+            {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+}
---
+++ modules/oauth/src/main/java/com/dyuproject/oauth/Transport.java
@@ -0,0 +1,177 @@
+//========================================================================
+//Copyright 2007-2009 David Yu dyuproject@gmail.com
+//------------------------------------------------------------------------
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at 
+//http://www.apache.org/licenses/LICENSE-2.0
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+//========================================================================
+
+package com.dyuproject.oauth;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+import com.dyuproject.util.ClassLoaderUtil;
+import com.dyuproject.util.Delim;
+import com.dyuproject.util.http.HttpConnector;
+import com.dyuproject.util.http.UrlEncodedParameterMap;
+import com.dyuproject.util.http.HttpConnector.Response;
+
+/**
+ * Transport - the means of sending the oauth request parameters
+ * 
+ * @author David Yu
+ * @created Jun 1, 2009
+ */
+
+public abstract class Transport
+{
+    
+    private static final Map<String,Transport> __defaults = new HashMap<String,Transport>(5);
+    private static int __inputBuffer = Integer.getInteger("tranport.input_buffer", 128);
+    
+    public static void setInputBuffer(int inputBuffer)
+    {
+        __inputBuffer = inputBuffer;
+    }
+    
+    public static int getInputBuffer()
+    {
+        return __inputBuffer;
+    }
+    
+    public static void register(Transport transport)
+    {
+        __defaults.put(transport.getName(), transport);
+    }
+    
+    public static Transport get(String name)
+    {
+        return __defaults.get(name);
+    }
+    
+    static
+    {
+        register(HttpAuthTransport.__default);
+        register(HttpPostTransport.__default);
+        register(HttpGetTransport.__default);
+        String ext = System.getProperty("oauth.transports.ext");
+        if(ext!=null)
+        {
+            try
+            {
+                StringTokenizer tokenizer = new StringTokenizer(ext, ",;");
+                while(tokenizer.hasMoreTokens())
+                {
+                    register((Transport)ClassLoaderUtil.newInstance(tokenizer.nextToken().trim(), 
+                            Signature.class));
+                }
+            }
+            catch(Exception e)
+            {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+    
+    public static void appendToUrl(String key, String value, StringBuilder urlBuffer)
+    {
+        urlBuffer.append('&').append(key).append('=').append(Signature.encodeString(value));
+    }
+    
+    public static StringBuilder buildAuthUrl(String authUrl, Token token, String callbackUrl)
+    {
+        char separator = authUrl.indexOf('?')==-1 ? '?' : '&';
+        StringBuilder buffer = new StringBuilder()
+            .append(authUrl)
+            .append(separator)
+            .append(Constants.OAUTH_TOKEN)
+            .append('=')
+            .append(token.getKey());
+        return callbackUrl==null ? buffer : buffer.append('&').append(Constants.OAUTH_CALLBACK)
+                .append('=').append(Signature.encodeString(callbackUrl));
+    }
+    
+    public static String getAuthUrl(String authUrl, Token token, String callbackUrl)
+    {
+        return buildAuthUrl(authUrl, token, callbackUrl).toString();
+    }
+    
+    static Response parse(Response response, Token token) throws IOException
+    {
+        if(response.getStatus()==200)
+            parse(response.getInputStream(), token);
+        return response;
+    }
+    
+    static String getMessage(InputStream in) throws IOException
+    {
+        InputStreamReader reader = new InputStreamReader(in, Constants.ENCODING);
+        char[] cbuf = new char[__inputBuffer];
+        StringBuilder buffer = new StringBuilder(__inputBuffer);
+        for(int len=0; (len = reader.read(cbuf))!=-1; )
+            buffer.append(cbuf, 0, len);
+        return buffer.toString();
+    }
+    
+    static void parse(InputStream in, Token token) throws IOException
+    {
+        InputStreamReader reader = new InputStreamReader(in, Constants.ENCODING);
+        char[] cbuf = new char[__inputBuffer];
+        StringBuilder buffer = new StringBuilder(__inputBuffer);
+        for(int len=0; (len = reader.read(cbuf))!=-1; )
+            buffer.append(cbuf, 0, len);
+        
+        String[] pairs = Delim.AMPER.split(buffer);
+        String key=null, secret=null;
+        for(String pair : pairs)
+        {
+            int idx = pair.indexOf('=');
+            if(idx==-1)
+                continue;
+            String k = pair.substring(0, idx);
+            String v = UrlEncodedParameterMap.decode(pair.substring(idx+1), Constants.ENCODING);
+            if(key==null && Constants.OAUTH_TOKEN.equals(k))
+                key = v;
+            else if(secret==null && Constants.OAUTH_TOKEN_SECRET.equals(k))
+                secret = v;
+            else
+                token.setAttribute(k, v);
+        }
+        if(key!=null && secret!=null)
+            token.set(token.getState()+1, key, secret);
+    }
+    
+    public abstract Response send(UrlEncodedParameterMap params, Endpoint ep, Token token,
+            TokenExchange exchange, NonceAndTimestamp nts, Signature signature, 
+            HttpConnector connector) throws IOException;
+    
+    public abstract String getName();
+    public abstract String getMethod();    
+    protected abstract void handleOAuthParameter(String key, String value, StringBuilder buffer);
+    
+    protected void handleRequestParameter(String key, String value, StringBuilder buffer)
+    {
+        buffer.append('&').append(key).append('=').append(value);
+    }
+    
+    public void putDefaults(UrlEncodedParameterMap params, Endpoint ep, Token token, 
+            TokenExchange exchange, NonceAndTimestamp nts, Signature signature,  
+            StringBuilder oauthBuffer, StringBuilder requestBuffer)
+    {
+        exchange.put(params, ep, token);
+        nts.put(params, ep.getConsumerKey());
+        signature.generate(params, ep.getConsumerSecret(), token, this, oauthBuffer, requestBuffer);
+    }
+
+}
