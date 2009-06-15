88705ac926cd1b6f2a96c92076b726a43ae6a8d1
dyuproject <dyuproject@a19f40fd-8e45-0410-b92a-0965b367a759>
2009-06-15 05:15:08 +0000

    oauth service provider demo

---
+++ trunk/modules/demos/oauth-serviceprovider-servlet/src/main/java/com/dyuproject/demos/oauthserviceprovider/GetRequestTokenServlet.java
@@ -0,0 +1,43 @@
+//========================================================================
+//Copyright 2007-2009 David Yu dyuproject@gmail.com
+//------------------------------------------------------------------------
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at 
+//http://www.apache.org/licenses/LICENSE-2.0
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+//========================================================================
+
+package com.dyuproject.demos.oauthserviceprovider;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author David Yu
+ * @created Jun 15, 2009
+ */
+
+@SuppressWarnings("serial")
+public class GetRequestTokenServlet extends AbstractServiceProviderServlet
+{
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws IOException, ServletException
+    {
+        getServiceProvider().handleTokenRequest(request, response);
+    }
+    
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws IOException, ServletException
+    {
+        getServiceProvider().handleTokenRequest(request, response);
+    }
+}
